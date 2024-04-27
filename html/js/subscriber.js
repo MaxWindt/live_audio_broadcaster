795766848a47420aff735c9f02ab6f9707175127
MaxWindt <86522026+MaxWindt@users.noreply.github.com>
2024-04-27 21:10:52 +0200

    Channels are only shown when server is ready

--- html/js/subscriber.js
+++ html/js/subscriber.js
@@ -1,71 +1,77 @@
-
-var getChannelsId = setInterval(function() {
-	console.log("get_channels");
-	let val = {Key: 'get_channels'}
-	wsSend(val);
+var getChannelsId = setInterval(function () {
+  console.log("get_channels");
+  let val = { Key: "get_channels" };
+  wsSend(val);
 }, 1000);
 
-document.getElementById('reload').addEventListener('click', function() {
-	window.location.reload(false);
+document.getElementById("reload").addEventListener("click", function () {
+  window.location.reload(false);
 });
 
 function channelClick(e) {
-	document.getElementById('output').classList.remove('hidden');
-	document.getElementById('channels').classList.add('hidden');
-	document.getElementById('reload').classList.remove('hidden');
-	let params = {};
-	params.Channel = e.target.innerText;
-	let val = {Key: 'connect_subscriber', Value: params};
-	wsSend(val);
-};
+  document.getElementById("output").classList.remove("hidden");
+  document.getElementById("channels").classList.add("hidden");
+  document.getElementById("reload").classList.remove("hidden");
+  let params = {};
+  params.Channel = e.target.innerText;
+  let val = { Key: "connect_subscriber", Value: params };
+  wsSend(val);
+}
 
 function updateChannels(channels) {
-	let channelsEle = document.querySelector('#channels ul');
-	channelsEle.innerHTML = '';
-	if(channels.length > 0) {
-		clearInterval(getChannelsId);
-		document.getElementById('nochannels').classList.add('hidden');
-		channels.forEach((e) => {
-			let c = document.createElement("li");
-			c.classList.add('channel');
-			c.innerText = e;
-			c.addEventListener("click", channelClick);
-			channelsEle.appendChild(c);
-		});
-	}
-	document.getElementById('channels').classList.remove('hidden');
+  let channelsEle = document.querySelector("#channels ul");
+  channelsEle.innerHTML = "";
+  if (channels.length > 0) {
+    clearInterval(getChannelsId);
+    document.getElementById("nochannels").classList.add("hidden");
+    channels.forEach((e) => {
+      let c = document.createElement("li");
+      c.classList.add("channel");
+      c.innerText = e;
+      c.addEventListener("click", channelClick);
+      channelsEle.appendChild(c);
+    });
+  }
+}
+
+ws.onmessage = function (e) {
+  let wsMsg = JSON.parse(e.data);
+  if ("Key" in wsMsg) {
+    switch (wsMsg.Key) {
+      case "info":
+        debug("server info:", wsMsg.Value);
+        break;
+      case "error":
+        //error("server error:", wsMsg.Value);
+        error(
+          "server error:",
+          "Server was not ready. Please try again, slower :)"
+        );
+        document.getElementById("output").classList.add("hidden");
+        document.getElementById("channels").classList.add("hidden");
+        break;
+      case "sd_answer":
+        startSession(wsMsg.Value);
+        break;
+      case "channels":
+        updateChannels(wsMsg.Value);
+        break;
+      case "session_established": // wait for the message that session_subscriber was received
+        document.getElementById("channels").classList.remove("hidden");
+        document.getElementById("spinner").classList.add("hidden");
+        console.log("session_established");
+        break;
+      case "ice_candidate":
+        pc.addIceCandidate(wsMsg.Value);
+        break;
+    }
+  }
 };
 
-ws.onmessage = function (e)	{
-	let wsMsg = JSON.parse(e.data);
-	if( 'Key' in wsMsg ) {
-		switch (wsMsg.Key) {
-			case 'info':
-				debug("server info:", wsMsg.Value);
-				break;
-			case 'error':
-				//error("server error:", wsMsg.Value);
-				error("server error:", "Server was not ready. Please try again, slower :)");
-				document.getElementById('output').classList.add('hidden');
-				document.getElementById('channels').classList.add('hidden');
-				break;
-			case 'sd_answer':
-				startSession(wsMsg.Value);
-				break;
-			case 'channels':
-				updateChannels(wsMsg.Value);
-				break;
-			case 'ice_candidate':
-				pc.addIceCandidate(wsMsg.Value)
-				break;
-		}
-	}
-};
-
-ws.onclose = function()	{
-	debug("WS connection closed");
-	pc.close()
-	document.getElementById('media').classList.add('hidden');
+ws.onclose = function () {
+  debug("WS connection closed");
+  pc.close();
+  document.getElementById("media").classList.add("hidden");
 };
 
 //
@@ -73,22 +79,24 @@ ws.onclose = function()	{
 //
 
 pc.ontrack = function (event) {
-	//console.log("Ontrack", event);
-	let el = document.createElement(event.track.kind);
-	el.srcObject = event.streams[0];
-	el.autoplay = true;
-	el.playsInline = true;
-	el.controls = true;
+  //console.log("Ontrack", event);
+  let el = document.createElement(event.track.kind);
+  el.srcObject = event.streams[0];
+  el.autoplay = true;
+  el.playsInline = true;
+  el.controls = true;
 
-	document.getElementById('media').appendChild(el);
-}
+  document.getElementById("media").appendChild(el);
+};
 
-pc.addTransceiver('audio')
+pc.addTransceiver("audio");
 
-pc.createOffer().then(d => {
-	pc.setLocalDescription(d)
-	let val = {Key: 'session_subscriber', Value: d};
-	wsSend(val);
-}).catch(debug);
+pc.createOffer()
+  .then((d) => {
+    pc.setLocalDescription(d);
+    let val = { Key: "session_subscriber", Value: d };
+    wsSend(val);
+  })
+  .catch(debug);
 
 // ----------------------------------------------------------------
