f1d3c7ae908caae6d76083bab76071d9b8f070c4
MaxWindt <86522026+MaxWindt@users.noreply.github.com>
2025-05-05 16:32:00 +0200

    added channel name to recording filename

--- html/js/publisher.js
+++ html/js/publisher.js
@@ -149,7 +149,9 @@ var handleStop = function () {
 var sendBlob = function (blob) {
   debug("sendBlob called, blob size:", blob.size);
   let date = new Date();
-  let filename = `recording_${date.toISOString().slice(0, 10)}_${
+  let filename = `${channelName.replace(/\s+/g, "_")}_${date.getFullYear()}${
+    date.getMonth() + 1 < 10 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1
+  }${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}_${
     date.getHours() < 10 ? "0" + date.getHours() : date.getHours()
   }_${
     date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes()
